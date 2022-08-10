fd6e6d6f405544b7c9e1050296976e43446f7c49
Abhishek Humagain <abhishek.humagain@innovatetech.co>
2022-08-11 00:39:44 +0545

    updated dockerfile and postgres connection string

--- SummerClass/Program.cs
+++ SummerClass/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using SummerClass.Infrastructure.DI;
+using SummerClass.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,41 @@ builder.Services.AddSwaggerGen();
 
 //Dependency Injection ==> refer to indrastructure
 builder.Services.AddInfrastructure(builder.Configuration);
+
+//add services to the container
+var defaultConnectionString = string.Empty;
+
+if (builder.Environment.EnvironmentName == "Development")
+{
+    defaultConnectionString = builder.Configuration.GetConnectionString("SummerClassDatabasePostgreSQL");
+}
+else
+{
+    // Use connection string provided at runtime by Heroku.
+    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+    var userPassSide = connectionUrl.Split("@")[0];
+    var hostSide = connectionUrl.Split("@")[1];
+
+    var user = userPassSide.Split(":")[0];
+    var password = userPassSide.Split(":")[1];
+    var host = hostSide.Split("/")[0];
+    var database = hostSide.Split("/")[1].Split("?")[0];
+
+    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+}
+
+var serviceProvider = builder.Services.BuildServiceProvider();
+try
+{
+    var dbContext = serviceProvider.GetRequiredService<ApplicationDBContext>();
+    dbContext.Database.Migrate();
+}
+catch
+{
+}
+
 
 var app = builder.Build();
 
