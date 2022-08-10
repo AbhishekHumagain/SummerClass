94ab3a99bd093a0e260823f53fb1e83cf88338c2
Abhishek Humagain <abhishek.humagain@innovatetech.co>
2022-08-10 22:14:44 +0545

    added salary or bonus table

---
+++ SummerClass.Infrastructure/Migrations/20220810162812_addedNewSalaryTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SummerClass.Infrastructure.Migrations
+{
+    public partial class addedNewSalaryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
